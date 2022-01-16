5e1ccbd0d0855a5bd4a9125c81fdbf22ef429db9
Rajesh Kumar <rajesh.k@avanzasolutions.com>
2022-01-16 21:57:11 +0500

    Initial Commit

---
+++ SignatureView/SignatureView.h
@@ -0,0 +1,18 @@
+//
+//  SignatureView.h
+//  SignatureView
+//
+//  Created by Rajesh Kumar Sahil on 16/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignatureView.
+FOUNDATION_EXPORT double SignatureViewVersionNumber;
+
+//! Project version string for SignatureView.
+FOUNDATION_EXPORT const unsigned char SignatureViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignatureView/PublicHeader.h>
+
+
---
+++ SignatureView/SignatureView.swift
@@ -0,0 +1,14 @@
+//
+//  SignatureView.swift
+//  SignatureView
+//
+//  Created by Rajesh Kumar Sahil on 16/01/2022.
+//
+
+import Foundation
+
+public class SignatureView{
+    public func hello(){
+        print("Hello Dear")
+    }
+}
